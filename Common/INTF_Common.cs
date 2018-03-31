643947d43d36907c9a4974cfc026a9a04cafdf79
iMAK73 <muginovak73@gmail.com>
2018-03-31 12:41:06 +0300

    MAK_310318-1 Sender Reciever moved to Common

--- Common/INTF_Common.cs
+++ Common/INTF_Common.cs
@@ -91,15 +91,27 @@ namespace Tanki
 
 
 
-	/// <summary> Интерфейс описывает сущность Отправляющюю сообщения клиенту/серверу</summary>
-	public interface ISender { }
+    /// <summary>
+    /// Cущность отправляющая информацию от клиента хосту
+    /// </summary>
+    public interface ISender
+    {
+        string RemoteAdress { get; set; }   // ip хоста
+        int RemotePort { get; set; }        // порт хоста
+        IPackage Pack { get; set; }         // пакет на отправку
+        void SendMessage();
+    }
 
 
-
-
-	/// <summary> Интерфейс описывает сущность Принимающюю сообщения от клиента/сервера </summary>
-	public interface IReceiver { }
-
+    /// <summary>
+    /// Cущность принимающая информацию клиентом от хоста
+    /// </summary>
+    public interface IReceiver
+    {
+        bool Alive { get; set; }   // работает ли поток на прием
+        int LocalPort { get; set; }        // прослушивающий порт
+        IPackage Run();
+    }
 
     #region MessageQueue Interfaces
     /// <summary> Интерфейс описывает очередь сообщений клиента/сервера </summary>
