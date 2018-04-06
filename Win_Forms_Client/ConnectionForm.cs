8d3c1e42cb534e9004bb8ad64859b74a62f4d080
Pavel Rem <PavelRemStep@gmail.com>
2018-04-06 19:17:43 +0300

    client_manage_engine

---
+++ Win_Forms_Client/ConnectionForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanki
+{
+	public partial class ConnectionForm : Form
+	{
+		Client client;
+		public ConnectionForm()
+		{
+			InitializeComponent();
+			textBox1.Text = "127.0.0.1";
+			textBox2.Text = "5000";
+			textBox3.Text = "5001";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string s1 = textBox1.Text;
+			string s2 = textBox2.Text;
+			string s3 = textBox3.Text;
+
+			if (!string.IsNullOrEmpty(s1)
+				&& !string.IsNullOrEmpty(s2)
+				&& !string.IsNullOrEmpty(s3))
+			{
+				IPAddress iP;
+				ushort remote_port;
+				ushort lockal_port;
+
+				if (!IPAddress.TryParse(s1, out iP))
+				{
+					label3.Text = "Не коректный Ip";
+					return;
+				}
+				if (!ushort.TryParse(s2, out remote_port) || remote_port > 65535)
+				{
+					label3.Text = "Не коректный порт сервера";
+					return;
+				}
+				if (!ushort.TryParse(s3, out lockal_port) || lockal_port > 65535)
+				{
+					label3.Text = "Не коректный локальный порт";
+					return;
+				}
+
+				IPEndPoint point = new IPEndPoint(iP, remote_port);
+
+				client = new Client(point, lockal_port);
+
+				if (Connect())
+				{
+					Lobby lobby = new Lobby(client);
+					lobby.Show();
+					Hide();
+				}
+			}
+			else label3.Text = "Error: Заполните все поля";
+		}
+
+		public bool Connect()
+		{
+			var caption = "Ошибка подключения";
+			var message = "Сервер не доступен";
+			var buttons = MessageBoxButtons.RetryCancel;
+			while (!client.RUN())
+			{
+				var result = MessageBox.Show(message, caption, buttons);
+				if (result == DialogResult.Cancel) { Close(); return false; }
+			}
+			return true;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+	}
+}
---
+++ Win_Forms_Client/ConnectionForm.designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Tanki
+{
+	partial class ConnectionForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(103, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(103, 56);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(25, 16);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "IP adress";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(25, 59);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(65, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Remote port";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(28, 165);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Cancel";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(128, 165);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Connect";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.ForeColor = System.Drawing.Color.Red;
+			this.label3.Location = new System.Drawing.Point(49, 138);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(0, 13);
+			this.label3.TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(103, 102);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(25, 106);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(54, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Local port";
+			// 
+			// ConnectionForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(237, 200);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "ConnectionForm";
+			this.Text = "ConnectionForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+	}
+}
