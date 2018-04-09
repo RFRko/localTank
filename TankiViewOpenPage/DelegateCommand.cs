ef8f66a8c855705cd91711a1d0a8d9b171e13511
ros1463 <ros1463@yandex.ru>
2018-04-09 22:45:51 +0300

    BaseView, DelegateCommand

---
+++ TankiViewOpenPage/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TankiViewOpenPage
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+
+        public DelegateCommand(Action<object> exec, Predicate<object> canExec = null)
+        {
+            this._execute = exec;
+            this._canExecute = canExec;
+        }
+    }
+}
