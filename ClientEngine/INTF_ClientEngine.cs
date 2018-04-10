8f1d6a8b6e395ca59f95d14dda9cd8bb72b75bff
iMAK73 <muginovak73@gmail.com>
2018-04-10 21:23:40 +0300

    add missing files

---
+++ ClientEngine/INTF_ClientEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Tanki
+{
+    public interface IClientEngine
+    {
+        IEnumerable<IRoomStat> RoomsStat { get; }
+        IMap Map { get; }
+        IEntity Entity { get; }
+
+        event EventHandler<RoomStatChangeData> OnRoomsStatChanged;
+        event EventHandler<IMap> OnMapChanged;
+        void OnEntityHandler(Object Sender, IEntity evntData);
+        void OnViewCommandHandler(Object Sender, Object evntData);
+
+    }
+}
+
---
+++ TankiServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IListener listener = new Listener(11001);
+            IServer Srv = new GameServer(listener);
+            Srv.RUN();
+
+
+
+
+        }
+    }
+}
