65957d61ea9b283bd24cde201a1287b38b33627b
xugo13 <ros1463@yandex.ru>
2018-04-12 20:08:54 +0300

    MainWindow edition, binding command

--- TankiViewOpenPage/MainWindowViewModel.cs
+++ TankiViewOpenPage/MainWindowViewModel.cs
@@ -12,7 +12,7 @@ namespace TankiViewOpenPage
     public class MainWindowViewModel: BaseViewModel
     {
         static IPAddress ipaddress = System.Net.IPAddress.Parse("127.0.0.1");
-        static int port = 8000;
+        static int port = 11001;
         static IPEndPoint loginEndPoint = new IPEndPoint(ipaddress, port);
 
         public IPAddress IpAddress
